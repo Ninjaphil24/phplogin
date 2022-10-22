8731f72fbcbd15fa8b0d8b54f98c56950837fcca
unknown <philipmodino@gmail.com>
2022-10-22 19:39:33 +0200

    lint test2

--- src/minMaxSum.php
+++ src/minMaxSum.php
@@ -1,25 +1,27 @@
 <?php
+
 /**
  * The file includes the time conversion function
- * 
+ *
  * PHP Version 8.1.6
- * 
+ *
  * @category Coding_Challenge
  * @package  HackerRank
  * @author   Philip Modinos <philipmodino@gmail.com>
  * @license  http://www.php.net/license/3_0.txt PHP License 3.0
  * @version  GIT: 1.0
- * @link 
- */ 
+ * @link
+ */
+
 $arr = [1, 3, 5, 7, 9];
 /**
  * Converts AM/PM to army time
  *
  * @param $arr is an array of different numbers
- * 
+ *
  * @author Philip Modinos
  * @return Min and Max Sum
- */ 
+ */
 function miniMaxSum($arr)
 {
 
@@ -38,7 +40,6 @@ function miniMaxSum($arr)
         return array_sum($arr);
     }
     echo minSum($arr) . " " . maxSum($arr);
-
 }
 
 echo miniMaxSum($arr);
