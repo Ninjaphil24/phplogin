6d8c07823d87d489c3f07bc572d2a162e8459de6
unknown <philipmodino@gmail.com>
2022-10-30 22:45:04 +0100

    php formatter

--- src/register.php
+++ src/register.php
@@ -1,69 +1,69 @@
-<?php 
-    //Import PHPMailer classes into the global namespace
-    //These must be at the top of your script, not inside a function
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
-    
-    //Load Composer's autoloader
-    require '../vendor/autoload.php';
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-    include 'config.php';
-    $msg = "";
-    if (isset($_POST['submit'])) {
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $password = mysqli_real_escape_string($conn, md5($_POST['password']));
-        $confirm_password = mysqli_real_escape_string($conn, md5($_POST['confirm-password']));
-        $code = mysqli_real_escape_string($conn, md5(rand()));
+//Load Composer's autoloader
+require '../vendor/autoload.php';
 
-        if (mysqli_num_rows(mysqli_query($conn, "SELECT*FROM   users WHERE email='{$email}'"))>0) {
-            $msg = "<div class = 'alert alert-danger'>{$email} - This email is already in use!</div>";
-        } else {
-                if ($password ===$confirm_password) {
-                    $sql = "INSERT INTO users (username, email,password,code) VALUES ('{$username}','{$email}', '{$password}','{$code}')";
-                    $result = mysqli_query($conn,$sql);
-                    if ($result) {
-                        echo "<div style='display:none;'>";
-                        //Create an instance; passing `true` enables exceptions
-                        $mail = new PHPMailer(true);
+include 'config.php';
+$msg = "";
+if (isset($_POST['submit'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $confirm_password = mysqli_real_escape_string($conn, md5($_POST['confirm-password']));
+    $code = mysqli_real_escape_string($conn, md5(rand()));
 
-                        try {
-                            //Server settings
-                            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                            $mail->isSMTP();                                            //Send using SMTP
-                            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                            $mail->Username   = 'automod24@gmail.com';                     //SMTP username
-                            $mail->Password   = 'phzneerhhwtwjfml';                               //SMTP password
-                            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-                            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    if (mysqli_num_rows(mysqli_query($conn, "SELECT*FROM   users WHERE email='{$email}'")) > 0) {
+        $msg = "<div class = 'alert alert-danger'>{$email} - This email is already in use!</div>";
+    } else {
+        if ($password === $confirm_password) {
+            $sql = "INSERT INTO users (username, email,password,code) VALUES ('{$username}','{$email}', '{$password}','{$code}')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                echo "<div style='display:none;'>";
+                //Create an instance; passing `true` enables exceptions
+                $mail = new PHPMailer(true);
 
-                            //Recipients
-                            $mail->setFrom('automod24@gmail.com', 'Phil Mod');
-                            $mail->addAddress($email);    
+                try {
+                    //Server settings
+                    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+                    $mail->isSMTP(); //Send using SMTP
+                    $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+                    $mail->SMTPAuth = true; //Enable SMTP authentication
+                    $mail->Username = 'automod24@gmail.com'; //SMTP username
+                    $mail->Password = 'phzneerhhwtwjfml'; //SMTP password
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+                    $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-                            //Content
-                            $mail->isHTML(true);                                  //Set email format to HTML
-                            $mail->Subject = 'Please do not reply to this email!';
-                            $mail->Body    = 'Please click on the following link to verify your registration <b><a href="http://localhost/phplogincest/?verification='.$code.'">http://localhost/phplogincest/?verification='.$code.'</a></b>';
+                    //Recipients
+                    $mail->setFrom('automod24@gmail.com', 'Phil Mod');
+                    $mail->addAddress($email);
 
-                            $mail->send();
-                            echo 'Message has been sent';
-                        } catch (Exception $e) {
-                            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                        }
-                        echo "</div>";
+                    //Content
+                    $mail->isHTML(true); //Set email format to HTML
+                    $mail->Subject = 'Please do not reply to this email!';
+                    $mail->Body = 'Please click on the following link to verify your registration <b><a href="http://localhost/phplogincest/?verification=' . $code . '">http://localhost/phplogincest/?verification=' . $code . '</a></b>';
 
-                        $msg = "<div class = 'alert alert-info'>We've sent a verification link to your email address.</div>";
-                    } else {
-                        $msg = "<div class = 'alert alert-danger>Something went wrong!</div>";
-                    }
-                } else {
-                    $msg = "<div class='alert alert-danger'>Password and Confirm Password don't match!</div>";
+                    $mail->send();
+                    echo 'Message has been sent';
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                 }
+                echo "</div>";
+
+                $msg = "<div class = 'alert alert-info'>We've sent a verification link to your email address.</div>";
+            } else {
+                $msg = "<div class = 'alert alert-danger>Something went wrong!</div>";
             }
+        } else {
+            $msg = "<div class='alert alert-danger'>Password and Confirm Password don't match!</div>";
+        }
     }
+}
 ?>
 
 
@@ -88,7 +88,7 @@
                 <input type="text" name="username" required>
                 <label>Username</label>
             </div>
-            
+
             <div class="inputBox">
                 <input type="email" name="email" required>
                 <label>Email</label>
@@ -104,16 +104,16 @@
                 <label>Passwort bestätigen</label>
             </div>
             <div class="buttons">
-          
+
           <button type="submit" name="submit" value="Submit">Registrieren</button>
           <a href="index.php">Login</a>
           </div>
-  
+
         </form>
 
 </div>
 
 
-    
+
 </body>
 </html>
