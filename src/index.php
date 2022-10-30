53d12ac06acc8a6eacfdc38acd87e870da716a0e
unknown <philipmodino@gmail.com>
2022-10-30 22:10:31 +0100

    Add styling login

--- src/index.php
+++ src/index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrierung</title>
+    <title>Login</title>
     <link rel="stylesheet" href="form.css">
     <link rel="shortcut icon" type="image/png" href="../images/favicon.png">
 </head>
 <body>
 <div class="box">
-        <h2>Registrierung</h2>
+        <h2>Login</h2>
         <form action="">
-            <div class="inputBox">
-                <input type="text" name="username" required>
-                <label>Username</label>
-            </div>
             
             <div class="inputBox">
                 <input type="email" name="email" required>
@@ -26,18 +22,13 @@
                 <input type="text" name="password" required>
                 <label>Passwort</label>
             </div>
-
-            <div class="inputBox">
-                <input type="text" name="confirm-password" required>
-                <label>Passwort bestätigen</label>
-            </div>
-            <div class="buttons">
+            
+        <div class="buttons">
           
-          <input type="submit" name="submit" value="Submit">
-          <a href="index.php">Login</a>
-          </div>
-  
-        </form>
+        <input type="submit" name="submit" value="Submit">
+        <a href="register.php">Registrierung</a>
+        </div>
+        </form> <br>
 
 </div>
 
