335cce498dac4ccc42b9fdf89ee24597a377177a
unknown <philipmodino@gmail.com>
2022-10-31 17:08:29 +0100

    Final 2

--- src/welcome.php
+++ src/welcome.php
@@ -9,13 +9,14 @@ $query = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$_SESSION['SES
 if (mysqli_num_rows($query) > 0) {
     $row = mysqli_fetch_assoc($query);
 
-?>
+    ?>
     <div class="box">
 
         <div style="width:320px">
-            <iframe src="https://giphy.com/embed/xUOrw1avEiJvQJlo76" width="320" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/xUOrw1avEiJvQJlo76" width="320" 
+                height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
         </div> <br>
         <h2><?php echo $row['username'] ?></h2> <br> <a href='logout.php'>Ausloggen</a>
     </div>
-<?php ;
+    <?php ;
 } ?>
